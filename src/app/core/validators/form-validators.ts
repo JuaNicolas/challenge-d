0b4a2c70717b426b3fa20a1d282490d3821a00a5
Nicolas Juanico <nicolas.juany@gmail.com>
2022-04-26 09:27:00 +0200

    Validators

--- src/app/core/validators/form-validators.ts
+++ src/app/core/validators/form-validators.ts
@@ -1,1 +1,51 @@
-export class FormValidators {}
+import { FormGroup, Validators } from '@angular/forms';
+
+export class FormValidators {
+  private static stringValidators() {
+    return [Validators.required];
+  }
+
+  static usernameValidators() {
+    return [Validators.required];
+  }
+
+  static nameValidators() {
+    return [Validators.required, Validators.minLength(2)];
+  }
+
+  static surnamesValidators() {
+    return [Validators.required, Validators.minLength(2)];
+  }
+
+  static emailValidators() {
+    return [Validators.required, Validators.email];
+  }
+
+  static matchPasswordsValidators() {
+    return (formGroup: FormGroup) => {
+      const passwordControl = formGroup.get('password');
+      const repeatPasswordControl = formGroup.get('repeatPasswordC');
+
+      if (!passwordControl || !repeatPasswordControl) {
+        return null;
+      }
+
+      return passwordControl.value === repeatPasswordControl.value
+        ? null
+        : { dismatch: true };
+    };
+  }
+
+  static passwordValidators() {
+    return [
+      Validators.required,
+      Validators.pattern(
+        '(?=.*d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@$!%*#?&^_-]).{8,}'
+      ),
+    ];
+  }
+
+  static ageValidators() {
+    return [Validators.required, Validators.min(18), Validators.max(65)];
+  }
+}
